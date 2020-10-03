cf212d1ff005f249fdc6ab150ad70ef1d9fbe10b
dogg0w0 <schlepper.felix@protonmail.ch>
2020-10-03 16:18:25 +0200

    fit_stack_all

--- output_files/fit_stack_all.C
+++ output_files/fit_stack_all.C
@@ -1,5 +1,12 @@
 {
     ROOT::Math::MinimizerOptions::SetDefaultMinimizer("Minuit2");
+    Int_t palette[5];
+    palette[0] = 2;
+    palette[1] = 3;
+    palette[2] = 4;
+    palette[3] = 6;
+    palette[4] = 7;
+    gStyle->SetPalette(5, palette);
     std::vector<TString> root_File_Names;
     for (int i = 0; i < 18; i++)
     {
@@ -36,7 +43,7 @@
         h_3->Add(h3);
         h_4->Add(h4);
     }
-     //Norm
+    //Norm
     Double_t scale_0 = 1 / h_0->Integral();
     Double_t scale_1 = 1 / h_1->Integral();
     Double_t scale_2 = 1 / h_2->Integral();
@@ -54,48 +61,73 @@
     h_3->SetMarkerStyle(kFullTriangleDown);
     h_4->SetMarkerStyle(kOpenCircle);
 
-    h_0->GetXaxis()->SetTitle("slope param");
+    h_0->GetXaxis()->SetTitle("slope parameter [current/luminosity]");
     h_0->GetYaxis()->SetTitle("n. entries");
     // FIT
-    Bool_t kfit = KFALSE;
+    Bool_t kfit = kTRUE;
     if (kfit)
     {
-    TF1 *g0 = new TF1("g2", "gaus", 0.09, 0.145);
-    TF1 *g4 = new TF1("g1", "gaus", 0.05, 0.105);
-    //g4->SetParLimits(0, 0, 17);
-    h_0->Fit(g0, "R");
-    h_4->Fit(g4, "R");
+        TF1 *g0 = new TF1("g0", "gaus", 0.09, 0.15);
+        TF1 *g1 = new TF1("g1", "gaus", 0.05, 0.12);
+        TF1 *g2 = new TF1("g2", "gaus", 0.03, 0.12);
+        TF1 *g3 = new TF1("g3", "gaus", 0.05, 0.12);
+        TF1 *g4 = new TF1("g4", "gaus", 0.05, 0.12);
+        h_0->Fit(g0, "R W");
+        h_1->Fit(g1, "R W");
+        h_2->Fit(g2, "R W");
+        h_3->Fit(g3, "R W");
+        h_4->Fit(g4, "R W");
     }
 
+    h_0->GetFunction("g0")->SetLineColor(palette[0]);
+    h_1->GetFunction("g1")->SetLineColor(palette[1]);
+    h_2->GetFunction("g2")->SetLineColor(palette[2]);
+    h_3->GetFunction("g3")->SetLineColor(palette[3]);
+    h_4->GetFunction("g4")->SetLineColor(palette[4]);
 
-    h_0->Draw("PLC PMC");
-    h_1->Draw("SAME PLC PMC");
-    h_2->Draw("SAME PLC PMC");
-    h_3->Draw("SAME PLC PMC");
-    h_4->Draw("SAME PLC PMC");
-    gPad->BuildLegend();
+    h_0->GetYaxis()->SetRangeUser(0, 0.4);
+    h_0->GetXaxis()->SetRangeUser(0.02, 0.15);
 
-    
-    //TLatex *tex = new TLatex();
-    //tex->SetNDC(kTRUE);
-    //tex->SetTextSize(0.035);
-    //tex->SetTextColor(kBlack);
-    //tex->DrawLatex(0.15, 0.85, "Gauss Fit Stack 0");
-    //char buffer_M[100],
-    //    buffer_S[100],
-    //    buffer_Chi[100];
-    //sprintf(buffer_M, "M_{Z} = %.3f #pm %.3f", g0->GetParameter(1), g0->GetParError(1));
-    //sprintf(buffer_S, "#sigma = %.3f #pm %.3f", g0->GetParameter(2), g0->GetParError(2));
-    //sprintf(buffer_Chi, "#chi^{2}_{red} = %.1f", g0->GetChisquare() / g0->GetNDF());
-    //tex->DrawLatex(0.15, 0.80, buffer_M);
-    //tex->DrawLatex(0.15, 0.75, buffer_S);
-    //tex->DrawLatex(0.15, 0.70, buffer_Chi);
-//
-    //tex->DrawLatex(0.15, 0.60, "Gauss Fit Stack 4");
-    //sprintf(buffer_M, "M_{Z} = %.3f #pm %.3f", g4->GetParameter(1), g4->GetParError(1));
-    //sprintf(buffer_S, "#sigma = %.3f #pm %.3f", g4->GetParameter(2), g4->GetParError(2));
-    //sprintf(buffer_Chi, "#chi^{2}_{red} = %.1f", g4->GetChisquare() / g4->GetNDF());
-    //tex->DrawLatex(0.15, 0.55, buffer_M);
-    //tex->DrawLatex(0.15, 0.50, buffer_S);
-    //tex->DrawLatex(0.15, 0.45, buffer_Chi);
+    h_0->Draw("9 PLC PMC");
+    h_1->Draw("9 SAME PLC PMC");
+    h_2->Draw("9 SAME PLC PMC");
+    h_3->Draw("9 SAME PLC PMC");
+    h_4->Draw("9 SAME PLC PMC");
+    gPad->BuildLegend(0.75, 0.75, 0.9, 0.9);
+
+    TLatex *tex = new TLatex();
+    tex->SetNDC(kTRUE);
+    tex->SetTextSize(0.02);
+    tex->SetTextColor(kBlack);
+    tex->DrawLatex(0.15, 0.88, "Gauss Fit Stack 0");
+    char buffer_M[100],
+        buffer_S[100];
+    sprintf(buffer_M, "M_{Z} = %.3f #pm %.3f", g0->GetParameter(1), g0->GetParError(1));
+    sprintf(buffer_S, "#sigma = %.3f #pm %.3f", g0->GetParameter(2), g0->GetParError(2));
+    tex->DrawLatex(0.15, 0.85, buffer_M);
+    tex->DrawLatex(0.15, 0.82, buffer_S);
+
+    tex->DrawLatex(0.15, 0.78, "Gauss Fit Stack 1");
+    sprintf(buffer_M, "M_{Z} = %.3f #pm %.3f", g1->GetParameter(1), g1->GetParError(1));
+    sprintf(buffer_S, "#sigma = %.3f #pm %.3f", g1->GetParameter(2), g1->GetParError(2));
+    tex->DrawLatex(0.15, 0.75, buffer_M);
+    tex->DrawLatex(0.15, 0.72, buffer_S);
+
+    tex->DrawLatex(0.15, 0.68, "Gauss Fit Stack 2");
+    sprintf(buffer_M, "M_{Z} = %.3f #pm %.3f", g2->GetParameter(1), g2->GetParError(1));
+    sprintf(buffer_S, "#sigma = %.3f #pm %.3f", g2->GetParameter(2), g2->GetParError(2));
+    tex->DrawLatex(0.15, 0.65, buffer_M);
+    tex->DrawLatex(0.15, 0.62, buffer_S);
+
+    tex->DrawLatex(0.15, 0.58, "Gauss Fit Stack 3");
+    sprintf(buffer_M, "M_{Z} = %.3f #pm %.3f", g3->GetParameter(1), g3->GetParError(1));
+    sprintf(buffer_S, "#sigma = %.3f #pm %.3f", g3->GetParameter(2), g3->GetParError(2));
+    tex->DrawLatex(0.15, 0.55, buffer_M);
+    tex->DrawLatex(0.15, 0.52, buffer_S);
+
+    tex->DrawLatex(0.15, 0.48, "Gauss Fit Stack 4");
+    sprintf(buffer_M, "M_{Z} = %.3f #pm %.3f", g4->GetParameter(1), g4->GetParError(1));
+    sprintf(buffer_S, "#sigma = %.3f #pm %.3f", g4->GetParameter(2), g4->GetParError(2));
+    tex->DrawLatex(0.15, 0.45, buffer_M);
+    tex->DrawLatex(0.15, 0.42, buffer_S);
 }
